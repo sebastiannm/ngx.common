1e1a72f3ea7c9beca599408139f57c229af85918
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-08-29 00:20:18 -0400

    added contentful index.ts

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export * from './src/directives/vote.directive';
 export * from './src/dynamic-image/dynamic-image.component';
 export * from './src/blog/safe-html.pipe';
 export * from './src/swiper/swiper.directive';
+export * from './src/contentful/contentful.service';
