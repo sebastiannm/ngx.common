5a86d800f52f9de321900baecf27b0d575944400
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 17:10:19 -0500

    1.0.32

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var blog_utils_1 = require("./src/blog/blog-utils");
+var blog_utils_1 = require("./blog/blog-utils");
 exports.BlogUtils = blog_utils_1.BlogUtils;
-var geoip_service_1 = require("./src/geoip/geoip.service");
+var geoip_service_1 = require("./geoip/geoip.service");
 exports.GeoIpService = geoip_service_1.GeoIpService;
-var dynamic_image_module_1 = require("./src/dynamic-image/dynamic-image.module");
+var dynamic_image_module_1 = require("./dynamic-image/dynamic-image.module");
 exports.DynamicImageModule = dynamic_image_module_1.DynamicImageModule;
-var safe_html_pipe_1 = require("./src/blog/safe-html.pipe");
+var safe_html_pipe_1 = require("./blog/safe-html.pipe");
 exports.SafeHtmlPipe = safe_html_pipe_1.SafeHtmlPipe;
-var swiper_directive_1 = require("./src/swiper/swiper.directive");
+var swiper_directive_1 = require("./swiper/swiper.directive");
 exports.SwiperDirective = swiper_directive_1.SwiperDirective;
-var contentful_service_1 = require("./src/contentful/contentful.service");
+var contentful_service_1 = require("./contentful/contentful.service");
 exports.ContentFulService = contentful_service_1.ContentFulService;
 //# sourceMappingURL=index.js.map
