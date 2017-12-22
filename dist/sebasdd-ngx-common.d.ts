68a69c6d639dffa13a4c0d0996ddad4eb966b111
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 16:33:20 -0500

    1.0.27 - simplify

--- dist/sebasdd-ngx-common.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { DynamicImageComponent as ɵa } from './src/dynamic-image/dynamic-image.component';
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { BlogUtils } from './blog/blog-utils';
-export { GeoIpService } from './geoip/geoip.service';
-export * from './dynamic-image/dynamic-image.module';
-export * from './blog/safe-html.pipe';
-export * from './swiper/swiper.directive';
