7411df28325555940ef25d8c8cdbfd25e8beca7a
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 17:11:51 -0500

    1.0.33

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { BlogUtils } from './blog/blog-utils';
-export { GeoIpService } from './geoip/geoip.service';
-export { DynamicImageModule } from './dynamic-image/dynamic-image.module';
-export { SafeHtmlPipe } from './blog/safe-html.pipe';
-export { SwiperDirective } from './swiper/swiper.directive';
-export { ContentFulService } from './contentful/contentful.service';
