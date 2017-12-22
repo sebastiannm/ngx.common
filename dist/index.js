d2df5f3f5ba5e44f4c7cb24437a302eb414e9792
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 11:45:47 -0500

    fixes import

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./blog/blog-utils"));
 __export(require("./geoip/geoip.service"));
-__export(require("./dynamic-image/dynamic-image.component"));
+__export(require("./dynamic-image/dynamic-image.module"));
 __export(require("./blog/safe-html.pipe"));
 __export(require("./swiper/swiper.directive"));
 __export(require("./contentful/contentful.service"));
