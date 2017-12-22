0ee0cf8ae310ecd315d1abc3b214ca86c2b9041b
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 16:40:36 -0500

    1.0.28

---
+++ dist/src/blog/safe-html.pipe.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var SafeHtmlPipe = /** @class */ (function () {
+    function SafeHtmlPipe(sanitizer) {
+        this.sanitizer = sanitizer;
+    }
+    SafeHtmlPipe.prototype.transform = function (value, args) {
+        if (!value) {
+            return '';
+        }
+        return this.sanitizer.bypassSecurityTrustHtml(value);
+    };
+    SafeHtmlPipe = __decorate([
+        core_1.Pipe({
+            name: 'pipe-safeHtml'
+        }),
+        __metadata("design:paramtypes", [typeof (_a = typeof platform_browser_1.DomSanitizer !== "undefined" && platform_browser_1.DomSanitizer) === "function" && _a || Object])
+    ], SafeHtmlPipe);
+    return SafeHtmlPipe;
+    var _a;
+}());
+exports.SafeHtmlPipe = SafeHtmlPipe;
+//# sourceMappingURL=safe-html.pipe.js.map
--- index.ts
+++
@@ -1,5 +0,0 @@
-export { BlogUtils } from './src/blog/blog-utils';
-export { GeoIpService } from './src/geoip/geoip.service';
-export { DynamicImageModule } from './src/dynamic-image/dynamic-image.module';
-export { SafeHtmlPipe } from './src/blog/safe-html.pipe';
-export { SwiperDirective } from './src/swiper/swiper.directive';
