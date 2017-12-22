b3f9b882013640d9154fee21d96d5f6219c3c789
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 09:55:04 -0500

    1.0.6

--- src/geoip/geoip.service.ts
+++ src/geoip/geoip.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/operator/share';
@@ -9,7 +9,7 @@ import 'rxjs/observable/of';
 @Injectable()
 export class GeoIpService {
 
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
 
   get() {
     const url: string = 'https://api.sebastianmoreno.se/api/geoip';
