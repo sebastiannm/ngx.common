3aa06bc5ca0a7d298c66d99b167c85b999617494
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 16:51:57 -0500

    1.0.30

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { BlogUtils } from './src/blog/blog-utils';
+export { GeoIpService } from './src/geoip/geoip.service';
+export { DynamicImageModule } from './src/dynamic-image/dynamic-image.module';
+export { SafeHtmlPipe } from './src/blog/safe-html.pipe';
+export { SwiperDirective } from './src/swiper/swiper.directive';
+export { ContentFulService } from './src/contentful/contentful.service';
