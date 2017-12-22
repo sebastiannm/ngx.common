a843af38ebde11cc7a7d9f46ff4479a3b87c3f5a
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 10:48:48 -0500

    1.0.14

--- src/dynamic-image/dynamic-image.component.ts
+++ src/dynamic-image/dynamic-image.component.ts
@@ -8,7 +8,183 @@ import { Component, Input, OnInit, OnDestroy, ElementRef } from '@angular/core';
     <img [src]="imgUrl" *ngIf="imgUrl" class="large"/>
   </div>
   `,
-  styleUrls: ['dynamic-image.sass']
+  styleUrls: [`
+    \:host {
+      display: block;
+    }
+
+    .dynamic-image-content {
+      position: relative;
+      overflow: hidden;
+      box-sizing: border-box;
+    }
+
+    .dynamic-image-content.absolute {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+
+    .dynamic-image-content pre {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: white;
+      padding: 0;
+      font-size: .75em;
+      line-height: 1.6em;
+    }
+
+    .dynamic-image-content img {
+      position: absolute;
+      padding: 0;
+      margin: 0;
+      line-height: 0;
+    }
+
+    .dynamic-image-content img.small {
+      opacity: 1;
+    }
+
+    .dynamic-image-content img.large {
+      opacity: 0;
+      transition: opacity .75s;
+    }
+
+    .dynamic-image-content.noplaceholder img.small {
+      visibility: hidden;
+    }
+
+    .dynamic-image-content.fit.height img {
+      height: 100%;
+    }
+
+    .dynamic-image-content.fit.width img {
+      width: 100%;
+    }
+
+    .dynamic-image-content.fit.autoheight {
+      line-height: 0;
+    }
+
+    .dynamic-image-content.fit.autoheight .spacer {
+      display: none;
+    }
+
+    .dynamic-image-content.fit.autoheight img {
+      position: relative;
+      left: initial;
+      right: initial;
+      bottom: initial;
+      top: initial;
+      transform: none;
+      height: 100%;
+      width: auto;
+    }
+
+    .dynamic-image-content.fit.autowidth img {
+      position: absolute;
+      width: 100%;
+    }
+
+    .dynamic-image-content.crop.height img {
+      height: 100%;
+      max-width: initial;
+    }
+
+    .dynamic-image-content.crop.width img {
+      width: 100%;
+      max-height: initial;
+    }
+
+    .dynamic-image-content.loaded img {
+      border: 0;
+    }
+
+    .dynamic-image-content.loaded img.large {
+      opacity: 1;
+    }
+
+    .dynamic-image-content.loaded img.small {
+      opacity: 0;
+      position: absolute;
+    }
+
+    .dynamic-image-content.center img {
+      left: 50%;
+      right: initial;
+      top: 50%;
+      bottom: initial;
+      transform: translateX(-50%) translateY(-50%);
+    }
+
+    .dynamic-image-content.left.center img {
+      top: 50%;
+      right: initial;
+      left: 0;
+      bottom: initial;
+      transform: translateX(0%) translateY(-50%);
+    }
+
+    .dynamic-image-content.right.center img {
+      top: 50%;
+      right: 0;
+      left: initial;
+      bottom: initial;
+      transform: translateX(0%) translateY(-50%);
+    }
+
+    .dynamic-image-content.left.top img {
+      top: 0;
+      right: initial;
+      bottom: initial;
+      left: 0;
+      transform: translateX(0%) translateY(0%);
+    }
+
+    .dynamic-image-content.center.top img {
+      top: 0;
+      right: initial;
+      bottom: initial;
+      left: 50%;
+      transform: translateX(-50%) translateY(0%);
+    }
+
+    .dynamic-image-content.right.top img {
+      top: 0;
+      right: 0;
+      bottom: initial;
+      left: initial;
+      transform: translateX(0%) translateY(0%);
+    }
+
+    .dynamic-image-content.left.bottom img {
+      top: initial;
+      right: initial;
+      bottom: 0;
+      left: 0;
+      transform: translateX(0%) translateY(0%);
+    }
+
+    .dynamic-image-content.center.bottom img {
+      top: initial;
+      right: initial;
+      bottom: 0;
+      left: 50%;
+      transform: translateX(-50%) translateY(0%);
+    }
+
+    .dynamic-image-content.right.bottom img {
+      top: initial;
+      right: 0;
+      bottom: 0;
+      left: initial;
+      transform: translateX(0%) translateY(0%);
+    }
+  `]
 })
 export class DynamicImageComponent implements OnInit, OnDestroy {
 
