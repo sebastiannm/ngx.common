d226433b3cf85d2a7b876c2c53feb3b9acd017a9
Sebastian Moreno <sebastiann.moreno@gmail.com>
2017-12-22 11:42:51 -0500

    dynamic image module

---
+++ src/dynamic-image/dynamic-image.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { DynamicImageComponent } from './dynamic-image.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [DynamicImageComponent],
+  providers: [],
+})
+export class DynamicImageModule { }
