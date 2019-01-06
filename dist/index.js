f7b25fbac0ddfa4618891ef662dafc83bb737c8a
Sebastian Moreno <sebastian@sebastianmoreno.se>
2019-01-06 12:02:04 -0500

    1.0.42

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var blog_utils_1 = require("./blog/blog-utils");
+exports.BlogUtils = blog_utils_1.BlogUtils;
+var geoip_service_1 = require("./geoip/geoip.service");
+exports.GeoIpService = geoip_service_1.GeoIpService;
+var dynamic_image_module_1 = require("./dynamic-image/dynamic-image.module");
+exports.DynamicImageModule = dynamic_image_module_1.DynamicImageModule;
+var safe_html_pipe_1 = require("./blog/safe-html.pipe");
+exports.SafeHtmlPipe = safe_html_pipe_1.SafeHtmlPipe;
+var swiper_directive_1 = require("./swiper/swiper.directive");
+exports.SwiperDirective = swiper_directive_1.SwiperDirective;
+var contentful_service_1 = require("./contentful/contentful.service");
+exports.ContentFulService = contentful_service_1.ContentFulService;
